77b83a70c76def36b51f942d2c9ef15bd706b5a3
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-02-08 19:04:53 +0200

    add new feature for file search program

--- search.py
+++ search.py
@@ -9,9 +9,11 @@ def search(name, time):
     for adresses, dirs, files in os.walk('/home/user/Desktop'):
         for file in files:
             full = os.path.join(adresses, file)
-            if name == file.split('.')[0] or ((TIME.time() - os.path.getctime(full)) < float(time)):
+            if name == file.split('.')[0] or\
+                    name == file or\
+                    ((TIME.time() - os.path.getctime(full)) < float(time)):
                 list_item.append(full)
     return list_item
 
-print(search(str(input('type the name of file or press enter to skip')),
-             (input('type the time in sec after creating file(s))'))))
+print(search(str(input('type the name of file or press enter to skip: ')),
+             (input('type the time in sec after creating file(s)): '))))
